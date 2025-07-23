d7d1f137ca20bb5c277b5b79e43024191fe8e099
ramsathwik <ramsathwikvutukuri630@gmail.com>
2025-07-23 18:16:37 +0530

    created todo version 2

---
+++ todo_2_version/src/App.jsx
@@ -0,0 +1,15 @@
+import Appname from "./components/Appname";
+import Addtodo from "./components/Search";
+import Todo1 from "./components/Todo1";
+import Todo2 from "./components/Todo2";
+function App() {
+  return (
+    <center class="todo-container">
+      <Appname></Appname>
+      <Addtodo></Addtodo>
+      <Todo1></Todo1>
+      <Todo2></Todo2>
+    </center>
+  );
+}
+export default App;
